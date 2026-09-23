dfc837d261f8ef3fb79d7a7259d17ba34fc6eb3d
Trung305 <trungkhac305@gmail.com>
2026-09-23 21:17:33 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,8 @@
+//Phạm Khắc Trung - 25610006
+fun main() {
+    val hoTen: String = "Phạm Khắc Trung"
+    val namSinh: Int = 2001
+    val namHienTai: Int = 2026
+    val tuoi: Int = namHienTai - namSinh
+    println("Tôi tên là : $hoTen, năm nay tôi $tuoi tuổi")
+}
