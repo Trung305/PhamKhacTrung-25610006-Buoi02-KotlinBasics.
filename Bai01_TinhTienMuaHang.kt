971e32eef763bc3e55bb966ff739ff82567413f9
Trung305 <trungkhac305@gmail.com>
2026-09-23 21:04:59 +0700

    Buoi02 Bai01 tinh tien mua hang

---
+++ Bai01_TinhTienMuaHang.kt
@@ -0,0 +1,9 @@
+//Phạm Khắc Trung - 25610006
+fun main() {
+    val soLuong :Int =6
+    val donGia :Double =7.5
+    val thanhTien :Double = soLuong.toDouble() * donGia
+    val tongTienSauThue: Double = thanhTien * 1.08
+    println("Thành tiền là: $thanhTien")
+    println("Tổng tiền sau thuế: $tongTienSauThue VND")
+}
