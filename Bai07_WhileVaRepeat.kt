26c2b7dd0ae3b31832e1379f1e11accad65c65c1
Trung305 <trungkhac305@gmail.com>
2026-09-24 19:46:09 +0700

    Buoi02 Bai07 dvong lap while va repeat

---
+++ Bai07_WhileVaRepeat.kt
@@ -0,0 +1,12 @@
+// Phạm Khắc Trung - 25610006
+fun main() {
+    var i:Int =10
+    while (i >= 0) {
+        println(i)
+        i--
+    }
+    println()
+    repeat(5) {
+        println("Dòng cố định")
+    }
+}
