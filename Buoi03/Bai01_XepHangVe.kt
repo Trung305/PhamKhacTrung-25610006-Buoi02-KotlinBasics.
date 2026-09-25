ddccd5bc62e5448d72ea7f4ca5fd36b1aa54e40d
Trung305 <trungkhac305@gmail.com>
2026-09-25 19:51:38 +0700

    Bai01 Xep hang ve

---
+++ Buoi03/Bai01_XepHangVe.kt
@@ -0,0 +1,6 @@
+//Phạm Khắc Trung - 25610006
+fun main() {
+    val tuoi = 20
+    val loaiVe = if (tuoi > 60) "Cao tuổi" else if (tuoi > 20) "Người lớn" else "Trẻ em"
+    println("Loại vé: $loaiVe")
+}
